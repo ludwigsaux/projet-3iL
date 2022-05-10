bb1928f5e8ac46bbca1299dd70f59e70b507291b
name <duperrav@3il.fr>
2022-05-10 10:43:59 +0200

    authentification

---
+++ src/users/users.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+
+// This should be a real class/interface representing a user entity
+export type User = any;
+
+
+@Injectable()
+export class UsersService {
+
+    private readonly users = [
+        {
+          userId: 1,
+          username: 'test1',
+          password: 'securite',
+        },
+        {
+          userId: 2,
+          username: 'test2',
+          password: 'security',
+        },
+      ];
+    
+      async findOne(username: string): Promise<User | undefined> {
+        return this.users.find(user => user.username === username);
+      }
+
+
+
+}
