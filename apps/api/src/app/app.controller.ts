fb9ee94f9d4ae075ed21f443b15545f3423008e8
name <duperrav@3il.fr>
2022-05-10 11:01:57 +0200

    Update app.controller.ts

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,15 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Request, Get, Post, UseGuards } from '@nestjs/common';
+
+import { AuthGuard } from '@nestjs/passport';
 
 import { Message } from '@dogemon/api-interfaces';
 
 import { AppService } from './app.service';
 
+import { GameController } from './game/game.controller';
+
+import { GamebotController } from './gamebot/gamebot.controller';
+
+
+
 @Controller()
 export class AppController {
+  
+  @UseGuards(AuthGuard('local'))
+  @Post('auth/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+
   constructor(private readonly appService: AppService) {}
 
   @Get('hello')
   getData(): Message {
     return this.appService.getData();
   }
+
+
 }
