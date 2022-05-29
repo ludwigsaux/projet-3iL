0433b0257308078fadc5c1b71aacae4b7f8dabac
ludwigsaux <75636940+ludwigsaux@users.noreply.github.com>
2022-05-29 19:27:57 +0200

    update

--- apps/api/src/app/creature/creature.controller.ts
+++ apps/api/src/app/creature/creature.controller.ts
@@ -15,10 +15,10 @@ constructor(private readonly creatureService: CreatureService) {}
 
 @Get('createcreature')
   async setCreatures(): Promise<void>{
-    this.creatureService.createCreature("droopy",'assets/img/droopy.jpg',100, 20);
-    this.creatureService.createCreature("scooby-doo",'assets/img/scooby.jpg',80, 30);  
+    this.creatureService.createCreature("bouvier",'assets/img/bouvier.png',100, 20);
+    this.creatureService.createCreature("scooby-doo",'assets/img/scooby.png',80, 30);  
     this.creatureService.createCreature("brian",'assets/img/brian.png',60, 20);  
-    this.creatureService.createCreature("clembapt",'assets/img/brian.png',60, 20);
+    this.creatureService.createCreature("dalmatien",'assets/img/dalmatien.png',60, 20);
   }
 
     @Get('creatures')
